7158d2338898056c38f25ac8158f7364c2ab2fc5
Navin Ayer <ayernavin@gmail.com>
2018-08-21 12:53:10 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# World Vision DVS
---
+++ server/scripts/run_develop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+. /venv/bin/activate
+pip3 install -r requirements.txt
+python manage.py migrate --no-input
+python manage.py runserver 0.0.0.0:8005
