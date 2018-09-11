644502c516e7b733769650cf8dc72829fe072d1d
Navin Ayer <ayernavin@gmail.com>
2018-09-11 14:47:39 +0545

    Add correspondence to report data
    * Add force extract command and migration

---
+++ server/report/management/commands/force_extract_reports.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from report.models import Report
+
+
+class Command(BaseCommand):
+    help = 'Force extract report data for all reports or provided report'
+
+    def handle(self, *args, **options):
+        for report in Report.objects.all():
+            report.extract_from_file()
+            report.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    'Successfully extract report "{}"'.format(report.name)
+                )
+            )
