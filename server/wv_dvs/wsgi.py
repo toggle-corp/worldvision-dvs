5eea71b5869ac4547a1cc1e2a20f79186aa36b3b
thenav56 <ayernavin@togglecorp.com>
2019-09-03 18:11:45 +0545

    Fix server build error

--- server/wv_dvs/wsgi.py
+++ server/wv_dvs/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wv_dvs.settings')
-
-from django.conf import settings # noqa E402
-sys.path.append(settings.APPS_DIR)
 
 application = get_wsgi_application()
