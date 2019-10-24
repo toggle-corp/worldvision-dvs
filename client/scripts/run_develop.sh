22e9590ee482f225952440093da167a79f18e996
thenav56 <ayernavin@togglecorp.com>
2019-10-24 14:14:46 +0545

    run_develop use pre-installed docker node modules

--- client/scripts/run_develop.sh
+++ client/scripts/run_develop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
 # Copy yarn node_modules from Image
-# rsync -a --stats --ignore-existing /node_modules /code/
-yarn install
+rsync -a --stats --ignore-existing /node_modules /code/
 yarn start
