f1ca76d7434804b83f94dd942c2a91240950bbbd
EBS <bkshrawanbk@gmail.com>
2020-08-28 17:16:26 +0545

    Add Dashboard GroupedBarChart

---
+++ client/src/views/Dashboard/GroupedBarChart/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+    LabelList,
+} from 'recharts';
+
+function CustomizedLabel(props) {
+    const { x, y, width, value } = props;
+    return (
+        <text
+            x={x + width / 2}
+            y={y}
+            dy={-8}
+            fontSize="12"
+        >
+            {value}
+        </text>
+    );
+}
+
+export default function GroupedBarChart(props) {
+    const {
+        data,
+    } = props;
+
+
+    if (data.values.length <= 0) {
+        return (
+            <div>Nothing to show</div>
+        );
+    }
+
+    return (
+        <ResponsiveContainer
+            height={250}
+            width="100%"
+        >
+            <BarChart
+                data={data.values}
+                margin={{ top: 50 }}
+            >
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend align="center" />
+                {data.columns.map(column => (
+                    <Bar
+                        dataKey={column}
+                        key={column}
+                        fill={data.colors[column]}
+                        label={<CustomizedLabel />}
+                    />
+                ))}
+            </BarChart>
+        </ResponsiveContainer>
+    );
+}
+
+GroupedBarChart.propTypes = {
+    data: PropTypes.object.isRequired,
+};
